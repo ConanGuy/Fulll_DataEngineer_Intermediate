94d330109458e1ee3a18fa9f359d2c07dc8c1fb2
ConanGuy <mathieuhuin@free.fr>
2024-09-29 13:29:43 +0200

    feat: completed ETL scripts

---
+++ etl.py
@@ -0,0 +1,32 @@
+from models import engine
+import pandas as pd
+
+CSV_FILE_PATH = 'retail_15_01_2022.csv'
+
+def extract():
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv(CSV_FILE_PATH)
+    return df
+
+def transform(df):
+    # Rename the columns
+    df = df.rename(columns={
+        'description': 'name',
+    })
+    
+    # Add transaction_date column
+    transaction_date = CSV_FILE_PATH.replace('.csv', '').replace('retail_', '')
+    transaction_day, transaction_month, transaction_year = transaction_date.split('_')
+    df['transaction_date'] = f'{transaction_year}-{transaction_month}-{transaction_day}'
+        
+    return df
+
+def load(df):
+    # Save the DataFrame to the database
+    df.to_sql('transactions', engine, if_exists='append', index=False)
+
+if __name__ == '__main__':
+    df = extract()
+    df = transform(df)
+    load(df)
+    
---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, BigInteger, Column, Float, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Création d'un moteur pour la base de données SQLite
+engine = create_engine('sqlite:///retail_conanguy.db') 
+Base = declarative_base()
+
+# Définition des classes de l'ORM
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    id = Column(String, primary_key=True)
+    transaction_date = Column(String)
+    category = Column(String)
+    name = Column(String)
+    quantity = Column(BigInteger)
+    amount_excl_tax = Column(Float)
+    amount_inc_tax = Column(Float)
+    
+if __name__ == '__main__':
+    # Drop the table if it already exists
+    Transaction.__table__.drop(engine, checkfirst=True)
+    Base.metadata.create_all(engine)
